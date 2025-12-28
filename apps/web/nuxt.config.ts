3b970781d6968af22f3875b087335f618a03b7ef
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2025-12-28 19:52:05 +0100

    chore: add payment layer with product fetching, pricing plans, and TypeScript support

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  extends: ['layer-dashboard', 'layer-testimonials', 'layer-blog'],
+  extends: [
+    'layer-dashboard',
+    'layer-testimonials',
+    'layer-blog',
+    'layer-payment',
+  ],
 
   nitro: {
     prerender: {
---
+++ packages/layer-payment/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { createNuxtSharedConfig } from 'eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default createNuxtSharedConfig(withNuxt)
